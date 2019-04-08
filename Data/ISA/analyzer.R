4d5bd4376c33d582e357ecc9d72c010c5b475bec
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2019-04-08 18:16:09 +0200

    Update analyzer.R

--- Data/ISA/analyzer.R
+++ Data/ISA/analyzer.R
@@ -32,8 +32,8 @@ morpher <- function(path_scontrino) {
 
 
 #path = "C:\\Users\\frtranquillo\\github\\tesi_repository\\Data\\ISA\\isareport16-19\\16-17-18"
-path = here("isareport16-19/merged")
-lista_scontrini <- as.list(list.files(path))
+path <- here("isareport16-19")
+lista_scontrini <- as.list(list.files(path,pattern = "*.txt"))
 
 # applico funzione morpher ad ogni elemento della lista di scontrini e poi unisco
 message("Analisi degli scontrini in corso...")
@@ -54,8 +54,8 @@ test$`TIPO CICLO` <-
     "DISINFEZIONE COMPLETA" = c("COMPLETE DISINFECTION", "DISINFEZIONE COMPLETA"),
     "DISINFEZIONE VELOCE" = c("DISINFEZIONE VELOCE", "FAST DISINFECTION"),
     "STERILIZZAZIONE COMPLETA"= c("COMPLETE STERILIZATION", "STERILIZZAZIONE COMPLETA"),
-    "STERILIZZAZIONE VELOCE" = c("STERILIZZAZIONE VELOCE" ,"FAST STERILIZATION","ster velo1"," STERILIZZAZIONE VELOCE"))
+    "STERILIZZAZIONE VELOCE" = c(" STERILIZAZIONE VELOCE" ,"FAST STERILIZATION","ster velo1"," STERILIZZAZIONE VELOCE"))
 table(test$`TIPO CICLO`)  
 
 #scrittura tabella
-write.csv2(test, file = here("isareport16-19/tabella_scontrini.csv"),row.names = F)
+write.csv2(test, file = here("tabella_scontrini.csv"),row.names = F)
