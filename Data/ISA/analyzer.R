a74144642705b850fd9461288f21f3ed3bef72ab
Tranquillo Francesco Raffaele <frtranquillo@aodv.local>
2019-04-10 16:21:39 +0200

    prima estensione all'analizzatore

--- Data/ISA/analyzer.R
+++ Data/ISA/analyzer.R
@@ -1,9 +1,47 @@
 library(tidyverse)
 library(dplyr)
+library(lubridate)
 library(here)
 library(pbapply)
 library(forcats)
+library(readr)
 
+
+path1 = here("isareport16-19")
+scontrino1 <- readLines(
+  paste0(
+    path1,
+    "\\",
+    "2016-03-16 11.51 53 Strumento-G120201 Operatore-C.G..txt"
+  ),
+  encoding = "UTF-8"
+)
+scontrino_footer1 <- scontrino1[16:(length(scontrino1) - 2)]
+
+path2 = here("isareport16-19")
+scontrino2 <- readLines(
+  paste0(
+    path2,
+    "\\",
+    "2018-04-16 14.51 3822 Strumento--B- G120516 Operatore-E.C.A..txt"
+  ),
+  encoding = "UTF-8"
+)
+scontrino_footer2 <- scontrino2[16:(length(scontrino2) - 2)]
+
+View(scontrino_footer1)
+View(scontrino_footer2)
+
+tempi <-
+  parse_time(substr(scontrino_footer1, 1, 8), format = "%H:%M:%S")
+dt <- difftime(tempi, lag(tempi, default = first(tempi)))
+barplot(as.numeric(dt),
+        names.arg = paste0("t.", seq(1, length(dt), 1)),
+        main = "Tempo (in secondi) impiegato\nad eseguire il t-esimo processo")
+t.labels <- paste0("t.", seq(1, length(dt), 1))
+dt_table <- data.frame(cbind(t.labels, as.numeric(dt)),stringsAsFactors = F)
+tmydf_header = setNames(data.frame(t(dt_table[, -1])), dt_table[, 1])
+head(tmydf_header)
 # funzione che dato in input uno scontrino lo trasforma in una tabella
 morpher <- function(path_scontrino) {
   scontrino <- readLines(paste0(path, "\\", path_scontrino))
@@ -15,7 +53,7 @@ morpher <- function(path_scontrino) {
                                            "(?<=[A-Z]): ",
                                            perl = T)),
                           stringsAsFactors = F)
-  tmydf_header = setNames(data.frame(t(df_header[,-1])), df_header[, 1])
+  tmydf_header = setNames(data.frame(t(df_header[, -1])), df_header[, 1])
   head(tmydf_header)
   df <- tmydf_header
   
@@ -24,7 +62,7 @@ morpher <- function(path_scontrino) {
   df <- df %>%
     mutate("CICLO REGOLARE" = ifelse(any(
       grepl("CICLO IRREGOLARE", scontrino_footer) == TRUE
-    ),-1, 1))
+    ), -1, 1))
   
   return(df)
 }
